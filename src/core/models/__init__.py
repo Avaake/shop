0e6264d359ca3de83eaeb8c20b0bc8dd3c3784ba
Avaake <133997731+Avaake@users.noreply.github.com>
2024-06-19 18:24:48 +0300

    create users table and new migration

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = [
     "Base",
     "Product",
     "Category",
+    "User",
 ]
 
 from .db_helper import db_helper
 from .base import Base
 from .product import Product
 from .category import Category
+from .user import User
