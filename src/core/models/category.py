8e97d1302ce7daa5bd160f5c82a90703a4bad377
Avaake <133997731+Avaake@users.noreply.github.com>
2024-06-19 17:32:17 +0300

    create table categories

---
+++ src/core/models/category.py
@@ -0,0 +1,9 @@
+from .base import Base
+from .mixins import IntIdPkMixins
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import VARCHAR
+
+
+class Category(Base, IntIdPkMixins):
+    __tablename__ = "categories"
+    category: Mapped[str] = mapped_column(VARCHAR(100))
