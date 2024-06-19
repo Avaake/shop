724b54de80a8b404d793f696ec83cad7ce25e44a
Avaake <133997731+Avaake@users.noreply.github.com>
2024-06-19 17:32:48 +0300

    create module user

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,5 +1,11 @@
-__all__ = ["db_helper", "Base", "Product"]
+__all__ = [
+    "db_helper",
+    "Base",
+    "Product",
+    "Category",
+]
 
 from .db_helper import db_helper
 from .base import Base
 from .product import Product
+from .category import Category
