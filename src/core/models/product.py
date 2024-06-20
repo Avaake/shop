e726ca99b24d097ac3759c5a3728f4aa8d03b98e
Avaake <133997731+Avaake@users.noreply.github.com>
2024-06-20 16:28:01 +0300

    add category_id column in product tabl

--- src/core/models/product.py
+++ src/core/models/product.py
@@ -1,10 +1,20 @@
 from .base import Base
 from .mixins import IntIdPkMixins
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import VARCHAR
+from sqlalchemy import VARCHAR, ForeignKey
 
 
 class Product(Base, IntIdPkMixins):
     name: Mapped[str] = mapped_column(VARCHAR(100))
     descriptions: Mapped[str] = mapped_column(VARCHAR(500))
     price: Mapped[int]
+    category_id: Mapped[str] = mapped_column(ForeignKey("categories.id"))
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}(id={self.id}, name={self.name!r}, descriptions={self.descriptions!r}, "
+            f"price={self.price!r}, category_id={self.category_id!r},)"
+        )
+
+    def __repr__(self):
+        return str(self)
