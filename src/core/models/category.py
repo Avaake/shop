d3486bcab3c1033ce986cd51b13961daa7404976
Avaake <133997731+Avaake@users.noreply.github.com>
2024-06-20 18:46:39 +0300

    add relationship

--- src/core/models/category.py
+++ src/core/models/category.py
@@ -1,12 +1,18 @@
 from .base import Base
 from .mixins import IntIdPkMixins
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import VARCHAR
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .product import Product
 
 
 class Category(Base, IntIdPkMixins):
     __tablename__ = "categories"
     category_name: Mapped[str] = mapped_column(VARCHAR(100))
+
+    products: Mapped[list["Product"]] = relationship(back_populates="category")
 
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, category_name={self.category_name!r})"
--- src/core/models/product.py
+++ src/core/models/product.py
@@ -1,7 +1,11 @@
 from .base import Base
 from .mixins import IntIdPkMixins
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import VARCHAR, ForeignKey
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .category import Category
 
 
 class Product(Base, IntIdPkMixins):
@@ -9,6 +13,8 @@ class Product(Base, IntIdPkMixins):
     descriptions: Mapped[str] = mapped_column(VARCHAR(500))
     price: Mapped[int]
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+
+    category: Mapped["Category"] = relationship(back_populates="products")
 
     def __str__(self):
         return (
