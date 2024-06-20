d8434e68a809b3aef23db0131be6d20b41e5af9f
Avaake <133997731+Avaake@users.noreply.github.com>
2024-06-20 17:07:18 +0300

    rename category in category_name in categories tabl

--- src/core/models/category.py
+++ src/core/models/category.py
@@ -7,3 +7,9 @@ from sqlalchemy import VARCHAR
 class Category(Base, IntIdPkMixins):
     __tablename__ = "categories"
     category_name: Mapped[str] = mapped_column(VARCHAR(100))
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, category_name={self.category_name!r})"
+
+    def __repr__(self):
+        return str(self)
