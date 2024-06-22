c0a5e5f03ac7f06ec098d4198f5d772c9a058062
Avaake <133997731+Avaake@users.noreply.github.com>
2024-06-22 16:54:58 +0300

    add get methode get_user_by_email

--- src/api/user/views.py
+++ src/api/user/views.py
@@ -4,13 +4,14 @@ from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper
 from . import crud as user_crud
+from pydantic import EmailStr
 
 router = APIRouter(
     tags=["Users"],
 )
 
 
-@router.post("", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def register_user(
     session: Annotated[
         AsyncSession,
@@ -20,3 +21,15 @@ async def register_user(
 ):
     user = await user_crud.create_user(session=session, user_in=user)
     return {"user": f"{user.username, user.email} register"}
+
+
+@router.get("/{email}")
+async def get_user_by_email(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+    email: EmailStr,
+):
+    user = await user_crud.get_user_by_email(session=session, email=email)
+    return user
